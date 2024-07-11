7512cb99efef9650f695a4f2f6699bd85e5a127c
Anandu Sankar <ananduap74@gmail.com>
2024-07-11 13:58:07 +0530

    11/07/204

--- controllers/ChatBoxController.js
+++ controllers/ChatBoxController.js
@@ -136,7 +136,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 server.listen(PORT, () => {
   console.log(`Socket server is running on port ${PORT}`);
 });
