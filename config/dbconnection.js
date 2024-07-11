e66ba53f66c9bd64dd1fa079f8c36238b840b0fe
Anandu Sankar <ananduap74@gmail.com>
2024-07-11 16:11:11 +0530

    09/07/2024

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    mongoose.connect(process.env.dburi)
+    mongoose.connect(process.env.dburi, {UseNewUrlParser:true},{useUnifiedTopology:true})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 };
